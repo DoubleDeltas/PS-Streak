76a3febb5b099189b9505a4f320aaa9c4639522d
Bonwoong Ku <36094170+DoubleDeltas@users.noreply.github.com>
2023-10-17 21:29:50 +0900

    [Gold III] Title: 전단지 돌리기, Time: 568 ms, Memory: 70236 KB -BaekjoonHub

---
+++ 백준/Gold/19542. 전단지 돌리기/전단지 돌리기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer tk = null;
+
+	static int N, S, D, ans;
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception {
+		tk = new StringTokenizer(rd.readLine());
+		N = Integer.parseInt(tk.nextToken());
+		S = Integer.parseInt(tk.nextToken());
+		D = Integer.parseInt(tk.nextToken());
+
+		for (int i=0; i<=N; i++)
+			graph.add(new ArrayList<>());
+		
+		for (int i=0; i<N-1; i++) {
+			tk = new StringTokenizer(rd.readLine());
+			int x = Integer.parseInt(tk.nextToken());
+			int y = Integer.parseInt(tk.nextToken());
+			graph.get(x).add(y);
+			graph.get(y).add(x);
+		}
+		
+		visited = new boolean[N+1];
+		dfs(S);
+		System.out.println(ans);
+	}
+	
+	static int dfs(int v) {
+		if (visited[v]) return 0;
+		visited[v] = true;
+		int d = 0;
+		for (int n: graph.get(v)) {
+			int result = dfs(n);
+			if (result > D) ans += 2;
+			d = Math.max(d, result);
+		}
+		return d + 1;
+	}
+}
