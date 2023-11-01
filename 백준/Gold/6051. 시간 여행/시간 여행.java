4c8311dfee96dcd30872ad7cb5cbb2d9c7dc2bfb
Bonwoong Ku <36094170+DoubleDeltas@users.noreply.github.com>
2023-11-01 15:03:57 +0900

    [Gold II] Title: 시간 여행, Time: 424 ms, Memory: 36772 KB -BaekjoonHub

---
+++ 백준/Gold/6051. 시간 여행/시간 여행.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer tk = null;
+
+	static int N, K;
+	static char c;
+	static Node[] hist;
+	static Node head;
+	
+	public static void main(String[] args) throws Exception {
+		N = Integer.parseInt(rd.readLine());
+		
+		hist = new Node[N+1];
+		head = new Node(-1, null);
+		
+		for (int i=1; i<=N; i++) {
+			tk = new StringTokenizer(rd.readLine());
+			c = tk.nextToken().charAt(0);
+			if (tk.hasMoreTokens())
+				K = Integer.parseInt(tk.nextToken());
+			
+			hist[i] = head;
+			switch (c) {
+			case 'a':
+				head = new Node(K, head);
+				break;
+			case 's':
+				head = head.prev;
+				break;
+			case 't':
+				head = hist[K];
+				break;
+			}
+			sb.append(head.n).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+	
+	static class Node {
+		int n;
+		Node prev, next;
+		
+		Node(int n, Node prev) {
+			this.n = n;
+			this.prev = prev;
+			if (prev != null)
+				prev.next = this;
+		}
+	}
+}
