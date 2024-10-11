58ec7de1c7dd28c0b7bf90cb9f3df026aab40890
Bonwoong Ku <36094170+DoubleDeltas@users.noreply.github.com>
2024-10-12 03:25:37 +0900

    [Gold III] Title: 내리막 길, Time: 340 ms, Memory: 36420 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -13,10 +13,7 @@ public class Main {
     static int M, N;
     static int[][] map;
 
-    static int[][] memo;    // memo[y][x] = (0,0)~(y,x)까지 가기 위한 경우의 수
-    static boolean[][][] enqueued;  // enqueued[y][x][d]: (y, x)에 d 방향으로 들어온 적이 있음
-
-    static PriorityQueue<Coord> pq = new PriorityQueue<>();
+    static int[][] memo;    // memo[y][x] = (y,x)~(M-1,N-1)까지 가기 위한 경우의 수
 
     public static void main(String[] args) throws Exception {
         tk = new StringTokenizer(rd.readLine());
@@ -24,50 +21,30 @@ public class Main {
         N = Integer.parseInt(tk.nextToken());
 
         map = new int[M][N];
-        enqueued = new boolean[M][N][4];
         memo = new int[M][N];
 
         for (int y=0; y<M; y++) {
             tk = new StringTokenizer(rd.readLine());
             for (int x=0; x<N; x++) {
                 map[y][x] = Integer.parseInt(tk.nextToken());
+                memo[y][x] = -1;
             }
         }
 
-        memo[0][0] = 1;
-        pq.offer(new Coord(0, 0));
-
-        while (!pq.isEmpty()) {
-            Coord coord = pq.poll();
-            int y = coord.y;
-            int x = coord.x;
-
-            for (int d=0; d<4; d++) {
-                int ny = y + dy[d];
-                int nx = x + dx[d];
-                if (ny < 0 || ny >= M || nx < 0 || nx >= N) continue;
-                if (enqueued[y][x][d]) continue;
-                if (map[y][x] <= map[ny][nx]) continue;
-
-                memo[ny][nx] += memo[y][x];
-                enqueued[y][x][d] = true;
-                pq.offer(new Coord(ny, nx));
-            }
-        }
-
-        System.out.println(memo[M-1][N-1]);
+        memo[M-1][N-1] = 1;
+        System.out.println(dfs(0, 0));
     }
 
-    static class Coord implements Comparable<Coord> {
-        int y, x;
-        Coord(int y, int x) {
-            this.y = y;
-            this.x = x;
+    static int dfs(int y, int x) {
+        if (memo[y][x] != -1) return memo[y][x];
+        int sum = 0;
+        for (int d=0; d<4; d++) {
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+            if (ny < 0 || ny >= M || nx < 0 || nx >= N) continue;
+            if (map[y][x] <= map[ny][nx]) continue;
+            sum += dfs(ny, nx);
         }
-
-        @Override
-        public int compareTo(Coord o) {
-            return -Integer.compare(map[y][x], map[o.y][o.x]);
-        }
+        return memo[y][x] = sum;
     }
 }
