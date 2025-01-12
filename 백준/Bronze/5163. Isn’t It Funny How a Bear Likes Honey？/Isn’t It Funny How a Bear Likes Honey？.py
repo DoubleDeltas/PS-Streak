eebae8a10b6a91cfc7024cba4c4a433e607aa00e
Bonwoong Ku <36094170+DoubleDeltas@users.noreply.github.com>
2025-01-12 11:02:19 +0900

    [Bronze III] Title: Isn’t It Funny How a Bear Likes Honey?, Time: 112 ms, Memory: 110904 KB -BaekjoonHub

--- 백준/Bronze/5163. Isn’t It Funny How a Bear Likes Honey？/Isn’t It Funny How a Bear Likes Honey？.py
+++ 백준/Bronze/5163. Isn’t It Funny How a Bear Likes Honey？/Isn’t It Funny How a Bear Likes Honey？.py
@@ -1,9 +1,5 @@
-from math import pi
-A = 4*pi/3 / 1000
 K = int(input())
 for t in range(1, K+1):
-    b, w = [typ(arg) for typ, arg in zip([int, float], input().split())]
-    B = A * sum(float(input())**3 for _ in range(b))
-    print(f'Data Set {t}:')
-    print('Yes' if B >= w else 'No')
-    if t < K: print()
+ b,w=input().split()
+ B=.0041887902047863905*sum(float(input())**3 for _ in range(int(b)))
+ print(f'Data Set {t}:','YNeos'[B<float(w)::2]+'\n'[:t<K],sep='\n')
