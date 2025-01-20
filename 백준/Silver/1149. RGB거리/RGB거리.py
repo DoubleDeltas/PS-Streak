f79c9e530e103b452ad87babe68a081f4ede77c2
Bonwoong Ku <36094170+DoubleDeltas@users.noreply.github.com>
2025-01-21 06:29:21 +0900

    [Silver I] Title: RGB거리, Time: 100 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,3 +1,9 @@
-L=[0]*3
-exec("M=input().split();L=[int(M[i])+min(L[i-2],L[i-1])for i in[0,1,2]];"*int(input()))
-print(min(L))
+R,G,B=[*zip(*[map(int,s.split())for s in [*open(0)][1:]])]
+N=len(R)
+r,g,b=[0]*N,[0]*N,[0]*N
+for i in range(N):
+    p=(i-1)%N
+    r[i]=R[i]+min(g[p],b[p])
+    g[i]=G[i]+min(r[p],b[p])
+    b[i]=B[i]+min(r[p],g[p])
+print(min(r[-1],g[-1],b[-1]))
