aea1db90278e4b1cc9779677a40bc229d83442f5
Bonwoong Ku <36094170+DoubleDeltas@users.noreply.github.com>
2025-01-24 03:17:06 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 540 ms, Memory: 79404 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -12,7 +12,7 @@ public class Main {
 
     static final int ID = 1;
     static final int MOD = 1_000_000_007;
-    static int[] segtree, zerotree;
+    static int[] segtree;
 
     public static void main(String[] args) throws Exception {
         tk = new StringTokenizer(rd.readLine());
@@ -24,11 +24,10 @@ public class Main {
         else U = Integer.highestOneBit(N-1) << 1;
 
         segtree = new int[2*U];
-        zerotree = new int[2*U];
 
         Arrays.fill(segtree, ID);
         for (int i=0; i<N; i++)
-            update(i, Integer.parseInt(rd.readLine()));
+            updateSeg(i, Integer.parseInt(rd.readLine()));
 
         for (int i=0; i<M+K; i++) {
             tk = new StringTokenizer(rd.readLine());
@@ -36,27 +35,15 @@ public class Main {
             int b = Integer.parseInt(tk.nextToken());
             int c = Integer.parseInt(tk.nextToken());
             if (a == '1')
-                update(b-1, c);
+                updateSeg(b-1, c);
             else
                 sb.append(getProduct(0, U-1, b-1, c-1, 1)).append('\n');
         }
         System.out.println(sb);
     }
 
-    static void update(int idx, int to) {
-        int i = U+idx;
-        if (zerotree[i] == 0 && to == 0) {
-            updateZero(idx, true);
-            return;
-        }
-        if (zerotree[i] > 0 && to > 0)
-            updateZero(idx, false);
-        updateSeg(idx, to);
-    }
-
     static void updateSeg(int idx, int to) {
         int i = U+idx;
-        int prev = segtree[i];
         segtree[i] = to;
         while (i > 1) {
             i /= 2;
@@ -64,20 +51,11 @@ public class Main {
         }
     }
 
-    static void updateZero(int idx, boolean isZero) {
-        int i = U+idx;
-        zerotree[i] = isZero ? 1 : 0;
-        while (i > 1) {
-            zerotree[i/2] += isZero ? 1 : -1;
-            i /= 2;
-        }
-    }
-
     static int getProduct(int s, int e, int a, int b, int idx) {
         if (e < a || b < s)
             return ID;
         if (a <= s && e <= b)
-            return zerotree[idx] > 0 ? 0 : segtree[idx];
+            return segtree[idx];
         int m = (s + e) / 2;
         return times(getProduct(s, m, a, b, idx * 2), getProduct(m+1, e, a, b, idx * 2 + 1));
     }
